543887b529ef5f7b9e4d45594a6c0b81ee9d57cc
Ivan Kataitsev <ivan.kataitsev@gmail.com>
2012-12-21 16:21:36 +0400

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Airportus::Application.config.secret_token = '6c4f9c58e440fa2195ee617d98e74b80571c7c0f2b10a29dae22ff20042a948a51b9d9059fdb57e09d8ae767d3c1c557612934f87bd0fcb12c31cb6ddce0b2b0'
